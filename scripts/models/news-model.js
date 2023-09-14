c27fce1cb8f4ed8246edd5ad67d83fc42089d675
Priyanshu Rana <124857927+frozen-hammer@users.noreply.github.com>
2023-09-14 11:28:12 +0530

    Create news-model.js

---
+++ scripts/models/news-model.js
@@ -0,0 +1,13 @@
+
+class News{
+    constructor(img_link,title,date,link,desc,source){
+        this.img = img_link;
+        this.title = title;
+        this.date = date;
+        this.link = link;
+        this.desc = desc;
+        this.source = source;
+    }
+}
+
+export default News;
